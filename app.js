62bf196d064f4717e4f43a97c6c73007db52fbb5
davidh-studies <160047075+davidh-studies@users.noreply.github.com>
2025-08-03 23:09:07 +0100

    NEW FUNC: Added sequelize database connection with postgres and simple User model

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const express = require( 'express' );
-const dotenv  = require( 'dotenv' );
+const express       = require( 'express' );
+const dotenv        = require( 'dotenv' );
+const { Sequelize } = require( 'sequelize' );
 
 // Load .env into process.env
 dotenv.config();
 
-const app     = express();
-const port    = process.env.PORT || 4001;
-
-
-app.listen( port, () => {
-
-    console.log(`Server is listening on: http://localhost:${port}`);
+// Configure the Sequelize ORM to connect to Postgres database
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 });
+
+const User = require('./src/Models/User.js')(sequelize);
+
+// Configure the express app
+const app     = express();
+const PORT    = process.env.PORT || 4001;;
+
+const startServer = async () => {
+
+    try {
+        // Authenticate with the database first
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+
+        // Sync the tables
+        await User.sync();
+        console.log('Database synced.');
+
+        // Start the server
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+startServer();
---
+++ src/Models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+
+const fields = {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'standard'
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    salt: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastLogin: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}
+
+const options = {
+    modelName: 'User', // We need to choose the model name
+    tableName: 'Users' // Explicitly tell sequlize the table name
+}
+
+class User extends Model{}
+
+module.exports = ( sequelize ) => {
+
+    options.sequelize = sequelize;
+
+    User.init( fields, options );
+
+    return User;
+};
